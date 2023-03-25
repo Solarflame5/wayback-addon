f6d8bc0e85ae20668ed3266b721f4adaf223bd7b
Solarflame5 <47642904+Solarflame5@users.noreply.github.com>
2023-03-25 23:37:01 +0300

    first working version

--- context_menu.js
+++ context_menu.js
@@ -1,17 +1,32 @@
 browser.runtime.onInstalled.addListener(() => {
     browser.menus.create({
         "id": "earliest_capture",
-        "title": "Earliest archived version",
+        "title": "Travel to the earliest archived version of this page",
         "contexts": ["all"]
     });
     browser.menus.create({
         "id": "capture_calendar",
-        "title": "Pick a date",
+        "title": "Pick a date to travel to",
         "contexts": ["all"]
     });
     browser.menus.create({
         "id": "latest_capture",
-        "title": "Latest archived version",
+        "title": "Travel to the latest archived version of this page",
         "contexts": ["all"]
     });
+});
+browser.menus.onClicked.addListener((info, tab) => {
+    if (info.menuItemId === "earliest_capture") {
+        browser.tabs.create({
+            url: "https://web.archive.org/web/1995/" + info.pageUrl
+        });
+    } else if (info.menuItemId === "capture_calendar") {
+        browser.tabs.create({
+            url: "https://web.archive.org/web/*/" + info.pageUrl
+        });
+    } else if (info.menuItemId === "latest_capture") {
+        browser.tabs.create({
+            url: "https://web.archive.org/web/" + info.pageUrl
+        });
+    }
 });
