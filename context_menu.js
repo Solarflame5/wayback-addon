e57a8506dd3c2e05d9d52c3453fffe7b719bc296
Solarflame5 <47642904+Solarflame5@users.noreply.github.com>
2023-05-01 12:40:50 +0300

    add a button to save the page and fix context menu

--- context_menu.js
+++ context_menu.js
@@ -1,4 +1,4 @@
-browser.runtime.onInstalled.addListener(() => {
+function addonStartup() {
     browser.menus.create({
         "id": "earliest_capture",
         "title": "Travel to the earliest archived version of this page",
@@ -14,7 +14,13 @@ browser.runtime.onInstalled.addListener(() => {
         "title": "Travel to the latest archived version of this page",
         "contexts": ["all"]
     });
-});
+    browser.menus.create({
+        "id": "capture_page",
+        "title": "Freeze this page in time",
+        "contexts": ["all"]
+    });
+}
+
 browser.menus.onClicked.addListener((info, tab) => {
     if (info.menuItemId === "earliest_capture") {
         browser.tabs.create({
@@ -28,5 +34,12 @@ browser.menus.onClicked.addListener((info, tab) => {
         browser.tabs.create({
             url: "https://web.archive.org/web/" + info.pageUrl
         });
+    } else if (info.menuItemId === "capture_page") {
+        browser.tabs.create({
+            url: "https://web.archive.org/save/" + info.pageUrl
+        });
     }
 });
+
+browser.runtime.onInstalled.addListener((addonStartup));
+browser.runtime.onStartup.addListener((addonStartup));
